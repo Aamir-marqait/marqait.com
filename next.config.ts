ad24e82eb08b0c3cee2d91774880cf2107e59c85
md-anas-sabah <mdanassabah@gmail.com>
2025-08-28 11:48:53 +0530

    updated Meta title, Meta description and H1 around the pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/products',
+        destination: '/ai-tools',
+        permanent: true,
+      },
+      {
+        source: '/blog',
+        destination: '/ai-blog',
+        permanent: true,
+      },
+      {
+        source: '/contacts',
+        destination: '/contact',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
