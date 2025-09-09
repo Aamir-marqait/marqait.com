011678f899ff734d362a4c091de3cebf4c0fc273
md-anas-sabah <mdanassabah@gmail.com>
2025-09-09 16:13:57 +0530

    Fixing

--- components/home/design-pattern.tsx
+++ components/home/design-pattern.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 export default function DesignPattern() {
   return (
-    <div
-      className="w-screen h-screen mt-3 pointer-events-none"
-      style={{
-        backgroundImage: "url(/svg/background-pattern.svg)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }}
-    ></div>
+    <div className="w-screen h-screen mt-3 pointer-events-none opacity-40 relative">
+      {/* Base SVG Pattern */}
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage: "url(/svg/background-pattern.svg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+    </div>
   );
 }
