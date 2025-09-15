fa08be212158dc51426d0be52878269dda1701b6
md-anas-sabah <mdanassabah@gmail.com>
2025-09-15 17:43:52 +0530

    fixing

--- app/ai-solution/seo/page.tsx
+++ app/ai-solution/seo/page.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Image from "next/image";
 import HeroSection from "./hero-section";
 import WhatIsAiSeo from "./what-is-seo";
 
 function page() {
   return (
-    <div className="">
+    <div className="bg-black">
       <HeroSection />
+      <div className="relative">
+        <Image
+          src="/glowing.webp"
+          alt="Glowing Effect"
+          width={1000}
+          height={700}
+          className="absolute -top-96 blur-2xl"
+        />
+      </div>
       <WhatIsAiSeo />
     </div>
   );
