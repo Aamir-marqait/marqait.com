3db4df86ba8a1a2762f00bfba43c35734b10ad88
md-anas-sabah <mdanassabah@gmail.com>
2025-09-16 16:18:43 +0530

    fixing

---
+++ app/ai-solution/ecommerce/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./hero-section";
+
+function page() {
+  return (
+    <div className="bg-black">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default page;
