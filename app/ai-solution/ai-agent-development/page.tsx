c23d03f74007b7668feb22ff85faa46f6388035f
md-anas-sabah <mdanassabah@gmail.com>
2025-09-16 12:14:32 +0530

    #added: what is ai agent dev component

--- app/ai-solution/ai-agent-development/page.tsx
+++ app/ai-solution/ai-agent-development/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AiAgentHero from "./ai-agent-hero-section";
+import WhatIsAgent from "./what-is-agent";
 
 function page() {
   return (
     <div className="bg-black">
       <AiAgentHero />
+      <WhatIsAgent />
     </div>
   );
 }
