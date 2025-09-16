46e9aa50db2ad896f17515d6305de85c9600ac6d
md-anas-sabah <mdanassabah@gmail.com>
2025-09-16 12:46:39 +0530

    Fixing

--- app/ai-solution/ai-agent-development/page.tsx
+++ app/ai-solution/ai-agent-development/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import AiAgentHero from "./ai-agent-hero-section";
 import WhatIsAgent from "./what-is-agent";
+import CustomAiAgentsSection from "./custom-ai-agents-section";
+import Why from "./why";
 
 function page() {
   return (
     <div className="bg-black">
       <AiAgentHero />
       <WhatIsAgent />
+      <CustomAiAgentsSection />
+      <Why />
     </div>
   );
 }
