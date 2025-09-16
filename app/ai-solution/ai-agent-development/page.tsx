90a9871a583d9a093507461e1e5602c1de7e8a5d
md-anas-sabah <mdanassabah@gmail.com>
2025-09-16 12:07:19 +0530

    Added New Page

---
+++ app/ai-solution/ai-agent-development/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AiAgentHero from "./ai-agent-hero-section";
+
+function page() {
+  return (
+    <div className="bg-black">
+      <AiAgentHero />
+    </div>
+  );
+}
+
+export default page;
