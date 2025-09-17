50ebcace0be67b854065752b7bf1baf3dbff318a
md-anas-sabah <mdanassabah@gmail.com>
2025-09-17 14:36:03 +0530

    Added Files

--- app/ai-solution/customer-service/hero-section.tsx
+++ app/ai-solution/customer-service/hero-section.tsx
@@ -1,7 +1,7 @@
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
-export default function AiAgentHero() {
+export default function Hero() {
   return (
     <section className="relative py-20  overflow-hidden">
       {/* Background Image */}
@@ -52,7 +52,7 @@ export default function AiAgentHero() {
               letterSpacing: "0%",
             }}
           >
-            AI Agent Development
+            AI For Customer Service
           </span>
         </nav>
 
--- app/ai-solution/customer-service/page.tsx
+++ app/ai-solution/customer-service/page.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import AiAgentHero from "./ai-agent-hero-section";
-import WhatIsAgent from "./what-is-agent";
 import CustomAiAgentsSection from "./custom-ai-agents-section";
 import Why from "./why";
 import DevelopmentProcessSection from "./our-dev-process";
 import IndustriesSection from "./industries-section";
-import AIAgentBenefits from "./ai-agent-benefits";
-import FaqSection from "./ai-agent-faq-section";
+import AIAgentBenefits from "./benefits";
+import FaqSection from "./faq-section";
 import ContactSection from "@/components/ai-solution-contact";
+import What from "./what";
+import Hero from "./hero-section";
 
 function page() {
   return (
     <div className="bg-black">
-      <AiAgentHero />
-      <WhatIsAgent />
+      <Hero />
+      <What />
       <CustomAiAgentsSection />
       <Why />
       <DevelopmentProcessSection />
