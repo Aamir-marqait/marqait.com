d8dd8a63ff7f00552824afb3936294d421e21944
md-anas-sabah <mdanassabah@gmail.com>
2025-09-17 15:19:16 +0530

    Fiixng

--- app/custom-ai-solutions/page.tsx
+++ app/custom-ai-solutions/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Metadata } from "next";
 import CustomAiAgentsSection from "./custom-ai-agents-section";
 import Why from "./why";
 import DevelopmentProcessSection from "./our-dev-process";
@@ -8,6 +9,11 @@ import FaqSection from "./faq-section";
 import ContactSection from "@/components/ai-solution-contact";
 import HeroSection from "./hero-section";
 import What from "./what";
+
+export const metadata: Metadata = {
+  title: "Custom AI Solutions - AI Software Development Company in India",
+  description: "Marqait AI is a leading AI software development company in India. We offer custom AI solutions that automate repetitive tasks and increase productivity.",
+};
 
 function page() {
   return (
