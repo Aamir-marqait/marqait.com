d4aad45a39cc776da0aaa585510f29c1cc9e333e
md-anas-sabah <mdanassabah@gmail.com>
2025-09-20 16:43:25 +0530

    #fix:pointer-events-none

--- app/ai-for-ecommerce/page.tsx
+++ app/ai-for-ecommerce/page.tsx
@@ -11,8 +11,8 @@ import ContactSection from "@/components/ai-solution-contact";
 function page() {
   return (
     <div className="bg-black relative overflow-hidden">
-      <div className="z-1 absolute -left-/4 top-0 w-[40rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
-      <div className="z-1 absolute right-32 top-96 transform -translate-y-1/2 w-[50rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
+      <div className="pointer-events-none z-1 absolute -left-/4 top-0 w-[40rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
+      <div className="pointer-events-none z-1 absolute right-32 top-96 transform -translate-y-1/2 w-[50rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
       <HeroSection />
       <WhatIsEcommerce />
       <EcommerceSolutionsSection />
--- app/ai-for-startups/page.tsx
+++ app/ai-for-startups/page.tsx
@@ -11,8 +11,8 @@ import FaqSection from "./faq";
 function page() {
   return (
     <div className="bg-black relative overflow-hidden">
-      <div className="absolute -left-1/4 top-0 w-[40rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
-      <div className="absolute right-32 top-72 transform -translate-y-1/2 w-[50rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
+      <div className="pointer-events-none absolute -left-1/4 top-0 w-[40rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
+      <div className="pointer-events-none absolute right-32 top-72 transform -translate-y-1/2 w-[50rem] h-[40rem] bg-[#850DB8] opacity-20 rounded-full blur-3xl"></div>
       <HeroSection />
       <WhatIsStartup />
       <SolutionForStartup />
